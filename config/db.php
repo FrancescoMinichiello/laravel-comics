4c4d039d0d7eec4d6ee9e64bccd4e365133a628d
FrancescoMinichiello <fraps2278@gmail.com>
2024-10-22 15:27:11 +0200

    first commit

---
+++ config/db.php
@@ -0,0 +1,167 @@
+<?php
+return [
+    "pokemonList" => [
+        [
+            "nome" => "Bulbasaur",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+            "tipo" => ["Erba", "Veleno"],
+            "abilita" => ["Erbaiuto"],
+            "specie" => "Pokémon Seme",
+            "elementi" => ["Sole", "Nutrimento"],
+            "evoluzioni" => ["Ivysaur", "Venusaur"]
+        ],
+        [
+            "nome" => "Charmander",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+            "tipo" => ["Fuoco"],
+            "abilita" => ["Aiutofuoco"],
+            "specie" => "Pokémon Lucertola",
+            "elementi" => ["Fuoco", "Calore"],
+            "evoluzioni" => ["Charmeleon", "Charizard"]
+        ],
+        [
+            "nome" => "Squirtle",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+            "tipo" => ["Acqua"],
+            "abilita" => ["Acquaiuto"],
+            "specie" => "Pokémon Miniatura Tartaruga",
+            "elementi" => ["Acqua", "Oceano"],
+            "evoluzioni" => ["Wartortle", "Blastoise"]
+        ],
+        [
+            "nome" => "Pikachu",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+            "tipo" => ["Elettrico"],
+            "abilita" => ["Statico"],
+            "specie" => "Pokémon Topo",
+            "elementi" => ["Elettricità", "Tuoni"],
+            "evoluzioni" => ["Raichu"]
+        ],
+        [
+            "nome" => "Jigglypuff",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png",
+            "tipo" => ["Normale", "Folletto"],
+            "abilita" => ["Tenacia", "Vocechiara"],
+            "specie" => "Pokémon Pallone",
+            "elementi" => ["Suono", "Luce"],
+            "evoluzioni" => ["Wigglytuff"]
+        ],
+        [
+            "nome" => "Meowth",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png",
+            "tipo" => ["Normale"],
+            "abilita" => ["Raccolta", "Agitazione"],
+            "specie" => "Pokémon Gatto Gracile",
+            "elementi" => ["Ombra", "Silenzio"],
+            "evoluzioni" => ["Persian"]
+        ],
+        [
+            "nome" => "Psyduck",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png",
+            "tipo" => ["Acqua"],
+            "abilita" => ["Umidità", "Intrapasso"],
+            "specie" => "Pokémon Papero",
+            "elementi" => ["Acqua", "Confusione"],
+            "evoluzioni" => ["Golduck"]
+        ],
+        [
+            "nome" => "Machop",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/066.png",
+            "tipo" => ["Lotta"],
+            "abilita" => ["Dentistretti", "Nullodifesa"],
+            "specie" => "Pokémon Superpotenza",
+            "elementi" => ["Forza", "Pietra"],
+            "evoluzioni" => ["Machoke", "Machamp"]
+        ],
+        [
+            "nome" => "Geodude",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/074.png",
+            "tipo" => ["Roccia", "Terra"],
+            "abilita" => ["Vigore", "Testadura"],
+            "specie" => "Pokémon Roccia",
+            "elementi" => ["Terra", "Pietra"],
+            "evoluzioni" => ["Graveler", "Golem"]
+        ],
+        [
+            "nome" => "Magnemite",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/081.png",
+            "tipo" => ["Elettro", "Acciaio"],
+            "abilita" => ["Magnetismo", "Ponderazione"],
+            "specie" => "Pokémon Magnetico",
+            "elementi" => ["Elettromagnetismo", "Acciaio"],
+            "evoluzioni" => ["Magneton", "Magnezone"]
+        ],
+        [
+            "nome" => "Onix",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/095.png",
+            "tipo" => ["Roccia", "Terra"],
+            "abilita" => ["Vigore", "Testadura"],
+            "specie" => "Pokémon Serpente Roccia",
+            "elementi" => ["Pietra", "Terra"],
+            "evoluzioni" => ["Steelix"]
+        ],
+        [
+            "nome" => "Gengar",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png",
+            "tipo" => ["Spettro", "Veleno"],
+            "abilita" => ["Levitazione", "Corpofunesto"],
+            "specie" => "Pokémon Ombra",
+            "elementi" => ["Oscurità", "Veleno"],
+            "evoluzioni" => ["Haunter", "Gengar"]
+        ],
+        [
+            "nome" => "Eevee",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png",
+            "tipo" => ["Normale"],
+            "abilita" => ["Adattabilità", "Fugafacile"],
+            "specie" => "Pokémon Evoluzione",
+            "elementi" => ["Versatilità", "Potenziale"],
+            "evoluzioni" => ["Vaporeon", "Jolteon", "Flareon", "Espeon", "Umbreon", "Leafeon", "Glaceon", "Sylveon"]
+        ],
+        [
+            "nome" => "Snorlax",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png",
+            "tipo" => ["Normale"],
+            "abilita" => ["Immunità", "Voracità"],
+            "specie" => "Pokémon Pigro",
+            "elementi" => ["Riposo", "Forza"],
+            "evoluzioni" => ["Munchlax"]
+        ],
+        [
+            "nome" => "Dratini",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/147.png",
+            "tipo" => ["Drago"],
+            "abilita" => ["Mutapelle"],
+            "specie" => "Pokémon Drago",
+            "elementi" => ["Dragone", "Magia"],
+            "evoluzioni" => ["Dragonair", "Dragonite"]
+        ],
+        [
+            "nome" => "Togepi",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/175.png",
+            "tipo" => ["Folletto"],
+            "abilita" => ["Leggiadro", "Aiutofuoco"],
+            "specie" => "Pokémon Uovo",
+            "elementi" => ["Fortuna", "Protezione"],
+            "evoluzioni" => ["Togetic", "Togekiss"]
+        ],
+        [
+            "nome" => "Mareep",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/179.png",
+            "tipo" => ["Elettro"],
+            "abilita" => ["Statico", "Agitazione"],
+            "specie" => "Pokémon Lana",
+            "elementi" => ["Elettricità", "Lana"],
+            "evoluzioni" => ["Flaaffy", "Ampharos"]
+        ],
+        [
+            "nome" => "Mudkip",
+            "immagine" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png",
+            "tipo" => ["Acqua"],
+            "abilita" => ["Acquaiuto", "Umidità"],
+            "specie" => "Pokémon Pescefango",
+            "elementi" => ["Acqua", "Fango"],
+            "evoluzioni" => ["Marshtomp", "Swampert"]
+        ],
+    ],
+];
