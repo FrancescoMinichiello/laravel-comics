1c753d76c3dcc1034fe1c75c74c84f81b9b73812
FrancescoMinichiello <fraps2278@gmail.com>
2024-10-22 23:40:45 +0200

    Added header and footer

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,17 @@
-<header class="container">
+<header class="container-fluid bg-primary d-flex justify-content-center mb-4">
     <div class="row">
         <div class="col">
             <nav class="d-flex">
                 <div class="row p-3">
                     <div class="col">
                         <ul class="d-flex gap-4 align-items-center">
-                            <li><img class="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHT_6V7i5-f6NYvjVSjGCbGitfE2bLxFHzZg&s" alt=""></li>
-                            <a href="#"><li>Home</li></a>
-                            <a href="#"><li>Pokedex</li></a>
-                            <a href="#"><li>About</li></a>
+                            <li><img class="logo" src="https://static.vecteezy.com/system/resources/previews/027/127/571/non_2x/pokemon-logo-pokemon-icon-transparent-free-png.png" alt=""></li>
+                            <a href="#"><li><h2 class="p-2 m-0">Home</h2></li></a>
+                            <a href="#"><li><h2 class="p-2 m-0">Pokedex</h2></li></a>
+                            <a href="#"><li><h2 class="p-2 m-0">About</h2></li></a>
+                            <a href="#"><li><h2 class="p-2 m-0">Search</h2></li></a>
+                            <a href="#"><li><h5 class="p-2 m-0">Login</h5></li></a>
+                            <a href="#"><li><h5 class="p-2 m-0">Register</h5></li></a>
                         </ul>
                     </div>
                 </div>
